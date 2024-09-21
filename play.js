a2b22a9ab13324bb3b9560287cdebb0c0f0de978
JohnS300 <johnsofokleous@gmail.com>
2024-09-21 23:27:26 +0300

    Simple function and basic syntax

---
+++ play.js
@@ -0,0 +1,16 @@
+let name = 'Max';
+let age = 30;
+let hashobbies = true;
+
+function sumamrizeUSer (userName, userAge, userHasHobbies) {
+  return (
+    "User's name is " +
+    userName +
+    ', age is ' +
+    userAge +
+    ' and the user has hobbies: ' +
+    userHasHobbies
+  );
+}
+
+console.log(sumamrizeUSer(name,age,hashobbies));

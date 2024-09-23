24e248b929134510e47bad2005b77e5f5e7fac50
JohnS300 <johnsofokleous@gmail.com>
2024-09-23 23:15:14 +0300

    Different ways to declare a function

--- play.js
+++ play.js
@@ -2,6 +2,7 @@ let name = 'Max';
 let age = 30;
 let hashobbies = true;
 
+//Normal function
 function sumamrizeUSer (userName, userAge, userHasHobbies) {
   return (
     "User's name is " +
@@ -13,4 +14,46 @@ function sumamrizeUSer (userName, userAge, userHasHobbies) {
   );
 }
 
+//Anonymous function
+const sumamrizeUSer1 = function (userName, userAge, userHasHobbies) {
+    return (
+      "User's name is " +
+      userName +
+      ', age is ' +
+      userAge +
+      ' and the user has hobbies: ' +
+      userHasHobbies
+    );
+  }
+
+//Arrow function
+const sumamrizeUSer2 =  (userName, userAge, userHasHobbies) => {
+    return (
+      "User's name is " +
+      userName +
+      ', age is ' +
+      userAge +
+      ' and the user has hobbies: ' +
+      userHasHobbies
+    );
+  }
+
+//functions add and add1 are the same but written in different, shorter syntax
+const add = (a,b) =>{
+    return a + b;
+}
+const add1 = (a,b) => a + b ;
+
+//functions add2 and add3 are the same but written in different, shorter syntax. add3 syntax is the most commonly used when the variable is only one
+const add2 = (a) => a + 1 ;
+const add3 = a => a + 1 ;
+
+
+const addNumbers = () => 5 + 5;
+
+
+console.log()
+console.log()
+console.log()
+console.log()
 console.log(sumamrizeUSer(name,age,hashobbies));

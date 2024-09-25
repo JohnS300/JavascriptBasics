739f19ba9820f3440b40312e1aa3bb2c884a0c9b
JohnS300 <johnsofokleous@gmail.com>
2024-09-26 00:09:42 +0300

    The function call() and bind() method

--- objects.js
+++ objects.js
@@ -13,5 +13,10 @@ const person2 = {
     lastName : "Tyler",
 };
 
+//calls person1.fullName with person2 as an argument, this refers to person2, even if fullName is a method of person1
 let x = person1.fullName.call(person2);
 console.log(x);
+
+//The object2 borrows the fullname method from object1
+let z = person1.fullName.bind(person2);
+console.log(z());

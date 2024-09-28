c8f97248f9a3de5aad93070cde999a9bf1952da6
JohnS300 <johnsofokleous@gmail.com>
2024-09-28 21:51:40 +0300

    different ways of using for loops to work with arrays

--- for.js
+++ for.js
@@ -14,3 +14,29 @@ for (let x in person){
     console.log(person[x]);
 };
 
+console.log('\nDoing a little experiment.\n')
+
+const objectarray = [];
+
+//doing this with Keys for fun
+for (let z=0; z < Object.keys(person).length; z++){
+    objectarray.push(person[Object.keys(person)[z]]);
+    console.log(objectarray[z]);
+}
+
+const objectarray2 = [];
+
+//doing this like a normal human
+for (value of Object.values(person)){
+    objectarray2.push(value);
+    console.log(value);
+}
+
+console.log(objectarray2.join('\n'));
+
+console.log('\nFinding a solution to my question\n')
+//this way might be more usefull
+for (value of Object.values(person)){
+    objectarray2.push(value);
+    console.log(objectarray2[objectarray2.length-1]);
+}

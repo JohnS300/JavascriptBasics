a04d4c5bdbc88fa920ef59a1113f35a493dd2e51
JohnS300 <johnsofokleous@gmail.com>
2024-09-29 23:56:53 +0300

    How to create Maps

---
+++ maps.js
@@ -0,0 +1,18 @@
+//first way to create a map
+const fruits = new Map([
+    ["orange", 555],
+    ["apple", 900],
+]);
+
+//Second way to create a map
+const balls = new Map();
+
+balls.set("baseball",20);
+balls.set("football",90);
+
+console.log(fruits.get("orange"));
+console.log(balls.get("football"));
+
+console.log('What type is fruits? ' + (typeof fruits));
+console.log('Is balls an instance of Map? ' + (balls instanceof Map));
+

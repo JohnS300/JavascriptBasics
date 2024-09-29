57371c426c5d1c10ce531554e2fd3424ef602cd3
JohnS300 <johnsofokleous@gmail.com>
2024-09-29 22:28:24 +0300

    Loop While

--- while.js
+++ while.js
@@ -5,7 +5,12 @@ let text = "";
 while (cars[i]) {
   text += cars[i];
   console.log(text);
-  text +=', '
+  if (i == (cars.length - 1)){
+    i++;
+    continue;
+  }
+  text +=', ';
   i++;
 }
 
+console.log(text);

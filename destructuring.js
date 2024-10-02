e68715627b44a3a600a3865d46890f36567a76b8
JohnS300 <johnsofokleous@gmail.com>
2024-10-02 23:55:06 +0300

    Destructuring in JS

---
+++ destructuring.js
@@ -0,0 +1,16 @@
+//Destructuring does not change the original object. You simply export the values of an objectinto their own variables.
+const person1 = {
+    firstName : "Jay",
+    lastName : "Don",
+    fullName : function (){
+        return this.firstName + ' ' + this.lastName;
+    },
+};
+
+//Simple destructure
+//let {firstName , lastName} = person1;
+//console.log(firstName, lastName);
+
+//Destructure with default values for missing properties
+let {firstName , lastName, country = 'US'} = person1;
+console.log(firstName, lastName , country);

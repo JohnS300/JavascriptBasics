601ee9faf1b7fda7333d8670be29e2177c26b983
JohnS300 <johnsofokleous@gmail.com>
2024-10-07 23:14:12 +0300

    Throw,Try,Catch,Finally

--- errors.js
+++ errors.js
@@ -1,1 +1,21 @@
 //Throw,Try,Catch,Finally
+let state = true;
+const currentDate = new Date();
+const portal= (opened) =>{
+    console.log((opened = true) ? "The portal is opened" : "The portal is closed"); 
+};
+
+try{
+    if (currentDate.getHours() <= 12){
+        portal(state);
+    }else if ((currentDate.getHours() >= 12) && (currentDate.getHours() < 22 )){
+        portal(stete = false);
+    }else{
+        throw 'Portal is unstable';
+    }
+
+}catch(err){
+    console.log(err);
+}finally{
+    console.log("Let's give up and drink some beer");
+};

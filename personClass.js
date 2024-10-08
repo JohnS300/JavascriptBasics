3d775350796b346f62f1be381e62f54504ff0cab
JohnS300 <johnsofokleous@gmail.com>
2024-10-08 23:29:55 +0300

    Created new Person class with multiple ethnicities just for fun

---
+++ personClass.js
@@ -0,0 +1,17 @@
+//JavaScript Classes are templates for JavaScript Objects.
+
+class Person {
+
+    constructor(name,age,city = 'London', ...ethnicities){
+        this.name = name;
+        this.age = age;
+        this.city = city;
+        this.ethnicities = ethnicities.length > 0 ? ethnicities : ['English'];
+
+    }
+
+
+    getdetails(){
+        return `Details provided:\nName : ${this.name},\nAge : ${this.age},\nCity of residence : ${this.city},\nEthnicities : ${this.ethnicities.join(', ')}`;
+    };
+};

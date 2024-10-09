ffc28d4a6757a5de04306002302a0aef87fba57e
JohnS300 <johnsofokleous@gmail.com>
2024-10-09 23:48:23 +0300

    Playing around with Object methods

--- main.js
+++ main.js
@@ -1,4 +1,17 @@
 const Person = require('./class/person');
 
+
+// Create Source Object
 const john = new Person('John',19,'Nepal','Nepalise','Australian','English');
-console.log(john.getdetails());
+console.log(john.getdetails()+'\n');
+
+
+const alex = new Person();
+//Copies properties from a source object to a target object
+Object.assign(alex,john);
+//Changing an object property
+Object.defineProperty(alex,'name',{
+    value : 'Alex',
+    writable : true,
+});
+console.log(alex.getdetails());
